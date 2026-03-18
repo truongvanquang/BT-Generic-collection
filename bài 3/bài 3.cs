c8ab5bd034fa8b7a2393586298b0f08d38d71302
truongvanquang <truongvanquang662@gmail.com>
2026-03-18 22:38:16 +0700

    Create bài 3.cs

---
+++ bài 3/bài 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SV: Truong Van Quang - MSV: 2415053122232");
+        List<int> numbers = new List<int>();
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu {i + 1}: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine("So lon nhat: " + numbers.Max());
+        Console.ReadLine();
+    }
+}
