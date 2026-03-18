5ee0f00a3370093340f757dce1cba1a21bf17ace
truongvanquang <truongvanquang662@gmail.com>
2026-03-18 22:52:54 +0700

    Create bài 6.cs

---
+++ bài 6/bài 6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SV: Truong Van Quang - MSV: 2415053122232");
+        List<string> ds = new List<string> { "C#", "Java", "Python", "C++" };
+
+        Console.Write("Nhap ngon ngu lap trinh can kiem tra: ");
+        string ngonNgu = Console.ReadLine();
+
+        if (ds.Contains(ngonNgu))
+        {
+            Console.WriteLine("Co ton tai.");
+        }
+        else
+        {
+            Console.WriteLine("Khong ton tai.");
+        }
+        Console.ReadLine();
+    }
+}
