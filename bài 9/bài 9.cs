0c9765ca7b374de794ec3c5fb42f3151972c1814
truongvanquang <truongvanquang662@gmail.com>
2026-03-18 23:07:41 +0700

    Create bài 9.cs

---
+++ bài 9/bài 9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SV: Truong Van Quang - MSV: 2415053122232");
+        List<int> numbers = new List<int>();
+        Console.Write("Nhap so luong phan tu (co the nhap trung): ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu {i + 1}: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        // Tạo danh sách mới không chứa phần tử trùng bằng Linq Distinct
+        List<int> uniqueList = numbers.Distinct().ToList();
+
+        Console.WriteLine("Danh sach sau khi loai bo trung lap: " + string.Join(", ", uniqueList));
+        Console.ReadLine();
+    }
+}
