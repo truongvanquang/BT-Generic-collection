13481a12a677a6d91e010f1d5b7881f86ff9c122
truongvanquang <truongvanquang662@gmail.com>
2026-03-18 22:47:59 +0700

    Create bài 5.cs

---
+++ bài 5/bài 5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SV: Truong Van Quang - MSV: 2415053122232");
+        List<string> students = new List<string> { "Quang", "An", "Binh", "Chau", "Dong" };
+        Console.WriteLine("Danh sach SV hien tai: " + string.Join(", ", students));
+
+        Console.Write("Nhap ten sinh vien muon xoa: ");
+        string name = Console.ReadLine();
+
+        if (students.Remove(name))
+        {
+            Console.WriteLine("Xoa thanh cong.");
+        }
+        else
+        {
+            Console.WriteLine("Khong tim thay ten nay.");
+        }
+
+        Console.WriteLine("Danh sach sau khi xoa: " + string.Join(", ", students));
+        Console.ReadLine();
+    }
+}
