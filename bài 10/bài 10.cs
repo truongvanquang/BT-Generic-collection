16b977ec3fc30e20c0974d7e4c06c87574ceb54f
truongvanquang <truongvanquang662@gmail.com>
2026-03-18 23:11:55 +0700

    Create bài 10.cs

---
+++ bài 10/bài 10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SV: Truong Van Quang - MSV: 2415053122232");
+        List<int> numbers = new List<int>();
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu {i + 1}: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        if (numbers.Count > 0)
+            Console.WriteLine("Gia tri nho nhat trong danh sach la: " + numbers.Min());
+
+        Console.ReadLine();
+    }
+}
