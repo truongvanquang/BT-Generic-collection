bd7d1b1a9b1813a4923611a92320454c58dcd8e8
truongvanquang <truongvanquang662@gmail.com>
2026-03-18 23:38:13 +0700

    Create bài 17.cs

---
+++ bài 17/bài 17.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SV: Truong Van Quang - MSV: 2415053122232");
+        Console.WriteLine("--- Bai 17: Tim so xuat hien nhieu nhat ---");
+
+        List<int> numbers = new List<int>();
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        // Tìm số xuất hiện nhiều nhất bằng LINQ
+        if (numbers.Count > 0)
+        {
+            var result = numbers.GroupBy(x => x)
+                                .OrderByDescending(g => g.Count())
+                                .First();
+
+            Console.WriteLine($"\nSo xuat hien nhieu nhat la: {result.Key} (xuat hien {result.Count()} lan)");
+        }
+
+        Console.ReadLine();
+    }
+}
